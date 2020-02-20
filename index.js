b1c9b840a259626b9ea8bab07ebd24917e26e11a
combattardigrade <combattardigrade@gmail.com>
2020-02-19 21:55:34 -0600

    loop each day and download 2 types of reports & download preferences

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Selenium Webdriver
 const chrome = require('selenium-webdriver/chrome')
 const { Builder, By, Key, until } = require('selenium-webdriver')
+
+
 const request = require('request-promise-native')
 const poll = require('promise-poller').default
 const fs = require('fs')
-const moment = require('moment')
+const path = require('path')
+
 
 const URL = 'https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT'
 
 const config = {
     sitekey: '6Lc6rokUAAAAAJBG2M1ZM1LIgJ85DwbSNNjYoLDk',
-    pageurl: 'https://srienlinea.sri.gob.ec/comprobantes-electronicos-internet/pages/consultas/recibidos/comprobantesRecibidos.jsf?&contextoMPT=https://srienlinea.sri.gob.ec/tuportal-internet&pathMPT=Facturaci%F3n%20Electr%F3nica&actualMPT=Comprobantes%20electr%F3nicos%20recibidos%20&linkMPT=%2Fcomprobantes-electronicos-internet%2Fpages%2Fconsultas%2Frecibidos%2FcomprobantesRecibidos.jsf%3F&esFavorito=S',
+    pageurl: 'https://srienlinea.sri.gob.ec/comprobantes-electronicos-internet/pages/consultas/menu.jsf',
     apiKey: 'cb678ca06942cb3636f0c6410c1a258b',
     apiSubmitUrl: 'http://2captcha.com/in.php',
     apiRetrieveUrl: 'http://2captcha.com/res.php'
@@ -31,7 +34,7 @@ const initiateCaptchaRequest = async (apiKey) => {
     return JSON.parse(response).request
 }
 
-const pollForRequestResults = async (key, id, retries = 30, interval = 1500, delay = 15000) => {
+const pollForRequestResults = async (key, id, retries = 30, interval = 1500, delay = 5000) => {
     console.log(`Waiting for ${delay} miliseconds...`)
     await sleep(delay)
     return poll({
@@ -93,62 +96,135 @@ const login = async (driver, userConfig) => {
     // Click sidebar btn
     await driver.wait(until.elementLocated(By.xpath('//*[@id="sri-menu"]')), 15000)
     await driver.findElement(By.xpath('//*[@id="sri-menu"]')).click()
-    await sleep(1500)
+    await sleep(500)
+
+    // Click facturacion en linea btn
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[1]/a')), 15000)
     await driver.findElement(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[1]/a')).click()
     await sleep(500)
-    await driver.findElement(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[2]/div/p-panelmenusub/ul/li[2]/a')).click()
 
-    // Wait for Report page to load    
+    // Click Produccion btn
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[2]/div/p-panelmenusub/ul/li[3]/a')), 15000)
+    await driver.findElement(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[2]/div/p-panelmenusub/ul/li[3]/a')).click()
+    await sleep(500)
+
+    // Click Consultas btn
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[2]/div/p-panelmenusub/ul/li[3]/p-panelmenusub/ul/li[2]/a')), 15000)
+    await driver.findElement(By.xpath('//*[@id="mySidebar"]/p-panelmenu/div/div[5]/div[2]/div/p-panelmenusub/ul/li[3]/p-panelmenusub/ul/li[2]/a')).click()
+    await sleep(500)
+
+    // Wait for Consulta de comprobantes page
     await driver.wait(until.titleContains('SISTEMA DE COMPROBANTES'), 15000)
+    // Wait for `Comprobantes electronicos recibidos y/o emitidos` link to load
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="consultaDocumentoForm:panelPrincipal"]/ul/li[2]/a')), 15000)
+    await driver.findElement(By.xpath('//*[@id="consultaDocumentoForm:panelPrincipal"]/ul/li[2]/a')).click()
     await sleep(3000)
 
+    // Wait for Report seach page to load    
+    await driver.wait(until.titleContains('SISTEMA DE COMPROBANTES'), 15000)
+    // Wait for page element to load
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="tituloPagina"]/div/span[1]')), 15000)
 
 
     // Loop period
-    while ((userConfig.startDate.month != userConfig.endDate.month) && (userConfig.startDate.year != userConfig.endDate.year)) {
+    while ((parseInt(userConfig.startDate.day) != parseInt(userConfig.endDate.day)) || (parseInt(userConfig.startDate.month) != parseInt(userConfig.endDate.month)) || (parseInt(userConfig.startDate.year) != parseInt(userConfig.endDate.year))) {
 
-        // Initiate captcha request
-        console.log('Starting recaptcha solution request...')
-        const requestId = await initiateCaptchaRequest(config.apiKey)
+        if (userConfig.downloadEmitidos == true) {
+            // Get recibos emitidos
+            console.log('Getting `recibos emitidos...`')
+            // Initiate captcha request
+            console.log('Starting recaptcha solution request...')
+            let requestId = await initiateCaptchaRequest(config.apiKey)
 
-        // Select all days => option 1
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="frmPrincipal:dia"]/option[1]')), 15000)
-        await driver.findElement(By.xpath('//*[@id="frmPrincipal:dia"]/option[1]')).click()
+            // Prepare date string
+            let day = (userConfig.startDate.day.toString()).length == 1 ? '0' + userConfig.startDate.day.toString() : userConfig.startDate.day.toString()
+            let month = (userConfig.startDate.month.toString()).length == 1 ? '0' + userConfig.startDate.month.toString() : userConfig.startDate.month.toString()
+            let date = day + '/' + month + '/' + userConfig.startDate.year.toString()
+            // Clear date input
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:calendarFechaDesde_input"]')).clear()
+            // Write date
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:calendarFechaDesde_input"]')).sendKeys(date)
 
-        // Select month
-        await driver.wait(until.elementLocated(By.xpath('//*[@id="frmPrincipal:mes"]/option[' + parseInt(userConfig.startDate.month) + ']')), 15000)
-        await driver.findElement(By.xpath('//*[@id="frmPrincipal:mes"]/option[' + parseInt(userConfig.startDate.month) + ']')).click()
+            // Select receipt type = Emitidos
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:cmbProcesos"]/option[1]')).click()
+            await sleep(2000)
 
-        // Select year       
-        let yearSelector = parseInt(userConfig.startDate.year) == moment().format('Y') ? 1 : moment().format('Y') - parseInt(userConfig.startDate.year) + 1        
-        await driver.findElement(By.xpath('//*[@id="frmPrincipal:ano"]/option[' + parseInt(yearSelector) + ']')).click()
+            // Wait for Recaptcha solution
+            console.log('Waiting for recaptcha solution...')
+            let response = await pollForRequestResults(config.apiKey, requestId)
+            console.log(response)
 
-        // Wait for Recaptcha solution
-        console.log('Waiting for recaptcha solution...')
-        const response = await pollForRequestResults(config.apiKey, requestId)
-        console.log(response)
+            // Inject recaptcha
+            console.log('Injecting recaptcha solution...')
+            await driver.executeScript(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`)
+            await driver.executeScript(`rcBuscar();`)
+            await sleep(500)
 
-        // Inject recaptcha
-        console.log('Injecting recaptcha solution...')
-        await driver.executeScript(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`)
-        await driver.executeScript(`rcBuscar();`)
-        await sleep(500)
+            // Download files
+        }
 
-        // Download files
-        
+        if (userConfig.downloadRecibidos == true) {
+            // Get recibos recibidos
+            console.log('Getting `recibos recibidos...`')
+            // Initiate captcha request
+            console.log('Starting recaptcha solution request...')
+            requestId = await initiateCaptchaRequest(config.apiKey)
 
+            // Prepare date string
+            let day = (userConfig.startDate.day.toString()).length == 1 ? '0' + userConfig.startDate.day.toString() : userConfig.startDate.day.toString()
+            let month = (userConfig.startDate.month.toString()).length == 1 ? '0' + userConfig.startDate.month.toString() : userConfig.startDate.month.toString()
+            let date = day + '/' + month + '/' + userConfig.startDate.year.toString()
+            // Clear date input
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:calendarFechaDesde_input"]')).clear()
+            // Write date
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:calendarFechaDesde_input"]')).sendKeys(date)
+
+            // Select receipt type = Emitidos
+            await driver.findElement(By.xpath('//*[@id="frmPrincipal:cmbProcesos"]/option[2]')).click()
+            await sleep(2000)
+
+            // Wait for Recaptcha solution
+            console.log('Waiting for recaptcha solution...')
+            response = await pollForRequestResults(config.apiKey, requestId)
+            console.log(response)
+
+            // Inject recaptcha
+            console.log('Injecting recaptcha solution...')
+            await driver.executeScript(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`)
+            await driver.executeScript(`rcBuscar();`)
+            await sleep(5000)
+
+
+            // Download files        
+            try {
+                // check if file exists
+                await driver.findElement(By.xpath('//*[@id="frmPrincipal:tablaCompRecibidos:0:lnkXml"]'))
+                for (let i = 0; i < 10; i++) {
+                    await driver.wait(until.elementLocated(By.xpath('//*[@id="frmPrincipal:tablaCompRecibidos:' + i + ':lnkXml"]')), 5000)
+                    await driver.findElement(By.xpath('//*[@id="frmPrincipal:tablaCompRecibidos:' + i + ':lnkXml"]')).click()
+                    await sleep(500)
+                }
+            }
+            catch (e) {
+                console.log(e)
+                console.log('No files found...')
+            }
+        }
 
 
         // Update startDate
-        userConfig.startDate.month = parseInt(userConfig.startDate.month) + 1
-        if (userConfig.startDate.month > 12) {
-            userConfig.startDate.month = 1
-            userConfig.startDate.year = parseInt(userConfig.startDate.year) + 1
+        userConfig.startDate.day = parseInt(userConfig.startDate.day) + 1
+        if (parseInt(userConfig.startDate.day) > 31 || (parseInt(userConfig.startDate.day > 28 && parseInt(userConfig.startDate.month) == 2))) {
+            userConfig.startDate.day = 1
+            userConfig.startDate.month = parseInt(userConfig.startDate.month) + 1
+            if (userConfig.startDate.month > 12) {
+                userConfig.startDate.month = 1
+                userConfig.startDate.year = parseInt(userConfig.startDate.year) + 1
+            }
         }
 
         // Wait before next month
         await sleep(1500)
-
     }
 
     console.log('Done...')
@@ -163,6 +239,25 @@ start = async () => {
     // use personal profile
     // options.addArguments("--user-data-dir=C:/Users/tardigrade/AppData/Local/Google/Chrome/User Data/");
     //options.addArguments("--profile-directory=Default")
+
+    // Download directory
+    //options.addArguments('download.default_directory', path.join(__filename, 'downloads'))
+
+    // Capabilities
+    // https://github.com/seleniumhq/selenium/issues/2349#issuecomment-390216266
+
+    // setUserPreferences
+    // https://stackoverflow.com/questions/46592946/how-to-set-file-download-directory-using-node-js-selenium-chrome-driver?rq=1
+
+    // Chrome experimental preferences
+    // https://stackoverflow.com/questions/46937319/how-to-use-chrome-webdriver-in-selenium-to-download-files-in-python
+
+    options.setUserPreferences({
+        'download.default_directory': path.resolve('downloads'),
+        'download.prompt_for_download': false,
+        "download.directory_upgrade": true,
+        "safebrowsing.enabled": true
+    })
 
     // Read Json file
     const userConfig = JSON.parse(fs.readFileSync('config.json', 'utf8'))
--- test.js
+++ test.js
@@ -1,26 +1,23 @@
 
 const moment = require('moment')
-
-
+const path = require('path')
 const userConfig = {
     "user": {
         "username": "1722357629001",
         "password": "JUAN1722"
     },
     "startDate": {
-        "month": "10",
-        "year": "2019"
+        "day": "1",
+        "month": "2",
+        "year": "2020"
     },
     "endDate": {
+        "day": "2",
         "month": "2",
         "year": "2020"
     },
     "URL": "https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT"
 }
 
-const year = 2020
-let yearSelector = parseInt(userConfig.startDate.year) == parseInt(moment().format('Y')) ? 1 : parseInt(moment().format('Y')) - parseInt(userConfig.startDate.year) + 1
-
-userConfig.startDate.month = parseInt(userConfig.startDate.month) + 1
-
-console.log(userConfig.startDate.month)
+console.log(path.join(__filename, 'downloads'))
+console.log(path.resolve('downloads'))
